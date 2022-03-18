bdfa3e47109ef8279d3c0f98f652a569e8fecec9
aflopez72 <aflopezm2@hotmail.com>
2022-03-17 23:32:36 -0500

    first commit reactiveMongo

---
+++ src/main/java/org/acme/ReactiveFruitService.java
@@ -0,0 +1,40 @@
+package org.acme;
+
+
+import io.quarkus.mongodb.reactive.ReactiveMongoClient;
+import io.quarkus.mongodb.reactive.ReactiveMongoCollection;
+import io.smallrye.mutiny.Uni;
+import org.bson.Document;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.List;
+@ApplicationScoped
+public class ReactiveFruitService {
+
+
+    @Inject
+    ReactiveMongoClient mongoClient;
+
+    public Uni<List<Fruit>> list() {
+        return getCollection().find()
+                .map(doc -> {
+                    Fruit fruit = new Fruit();
+                    fruit.setName(doc.getString("name"));
+                    fruit.setDescription(doc.getString("description"));
+                    return fruit;
+                }).collect().asList();
+    }
+
+    public Uni<Void> add(Fruit fruit) {
+        Document document = new Document()
+                .append("name", fruit.getName())
+                .append("description", fruit.getDescription());
+        return getCollection().insertOne(document)
+                .onItem().ignore().andContinueWithNull();
+    }
+
+    private ReactiveMongoCollection<Document> getCollection() {
+        return mongoClient.getDatabase("fruit").getCollection("fruit");
+    }
+}
